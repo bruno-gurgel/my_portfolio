6b2362854aaa10e384b1eee3edd8fd3b6c12721b
Bruno Marques Gurgel <bmg1612@gmail.coml>
2023-03-08 23:31:23 -0300

    feat: updates generated types

--- __generated__/graphql.ts
+++ __generated__/graphql.ts
@@ -1,121 +1,370 @@
 /* eslint-disable */
-import { TypedDocumentNode as DocumentNode } from '@graphql-typed-document-node/core';
-export type Maybe<T> = T | null;
-export type InputMaybe<T> = Maybe<T>;
-export type Exact<T extends { [key: string]: unknown }> = { [K in keyof T]: T[K] };
-export type MakeOptional<T, K extends keyof T> = Omit<T, K> & { [SubKey in K]?: Maybe<T[SubKey]> };
-export type MakeMaybe<T, K extends keyof T> = Omit<T, K> & { [SubKey in K]: Maybe<T[SubKey]> };
+import { TypedDocumentNode as DocumentNode } from '@graphql-typed-document-node/core'
+export type Maybe<T> = T | null
+export type InputMaybe<T> = Maybe<T>
+export type Exact<T extends { [key: string]: unknown }> = {
+  [K in keyof T]: T[K]
+}
+export type MakeOptional<T, K extends keyof T> = Omit<T, K> & {
+  [SubKey in K]?: Maybe<T[SubKey]>
+}
+export type MakeMaybe<T, K extends keyof T> = Omit<T, K> & {
+  [SubKey in K]: Maybe<T[SubKey]>
+}
 /** All built-in and custom scalars, mapped to their actual values */
 export type Scalars = {
-  ID: string;
-  String: string;
-  Boolean: boolean;
-  Int: number;
-  Float: number;
+  ID: string
+  String: string
+  Boolean: boolean
+  Int: number
+  Float: number
   /** A date string, such as 2007-12-03, compliant with the `full-date` format outlined in section 5.6 of the RFC 3339 profile of the ISO 8601 standard for representation of dates and times using the Gregorian calendar. */
-  Date: any;
-};
+  Date: any
+}
 
 export type Comment = {
-  __typename?: 'Comment';
-  author: User;
-  content: Scalars['String'];
-  createdAt: Scalars['Date'];
-  id: Scalars['ID'];
-  post: Post;
-  updatedAt: Scalars['Date'];
-};
+  __typename?: 'Comment'
+  author: User
+  content: Scalars['String']
+  createdAt: Scalars['Date']
+  id: Scalars['ID']
+  post: Post
+  updatedAt: Scalars['Date']
+}
 
 export type Mutation = {
-  __typename?: 'Mutation';
-  createPost: Post;
-  createProject: Project;
-};
-
+  __typename?: 'Mutation'
+  createPost: Post
+  createProject: Project
+}
 
 export type MutationCreatePostArgs = {
-  authorId: Scalars['ID'];
-  content: Scalars['String'];
-  description: Scalars['String'];
-  slug: Scalars['String'];
-  title: Scalars['String'];
-};
-
+  authorId: Scalars['ID']
+  content: Scalars['String']
+  description: Scalars['String']
+  slug: Scalars['String']
+  title: Scalars['String']
+}
 
 export type MutationCreateProjectArgs = {
-  cover: Scalars['String'];
-  description: Scalars['String'];
-  link: Scalars['String'];
-  name: Scalars['String'];
-};
+  cover: Scalars['String']
+  description: Scalars['String']
+  link: Scalars['String']
+  name: Scalars['String']
+}
 
 export type Post = {
-  __typename?: 'Post';
-  author: User;
-  comments: Array<Comment>;
-  content: Scalars['String'];
-  createdAt: Scalars['Date'];
-  description: Scalars['String'];
-  id: Scalars['ID'];
-  published: Scalars['Boolean'];
-  slug: Scalars['String'];
-  title: Scalars['String'];
-  updatedAt: Scalars['Date'];
-};
+  __typename?: 'Post'
+  author: User
+  comments: Array<Comment>
+  content: Scalars['String']
+  createdAt: Scalars['Date']
+  description: Scalars['String']
+  id: Scalars['ID']
+  published: Scalars['Boolean']
+  slug: Scalars['String']
+  title: Scalars['String']
+  updatedAt: Scalars['Date']
+}
 
 export type Project = {
-  __typename?: 'Project';
-  cover: Scalars['String'];
-  createdAt: Scalars['Date'];
-  description: Scalars['String'];
-  id: Scalars['ID'];
-  link: Scalars['String'];
-  name: Scalars['String'];
-  updatedAt: Scalars['Date'];
-};
+  __typename?: 'Project'
+  cover: Scalars['String']
+  createdAt: Scalars['Date']
+  description: Scalars['String']
+  id: Scalars['ID']
+  link: Scalars['String']
+  name: Scalars['String']
+  updatedAt: Scalars['Date']
+}
 
 export type Query = {
-  __typename?: 'Query';
-  comments: Array<Comment>;
-  ok: Scalars['Boolean'];
-  posts: Array<Post>;
-  projects: Array<Project>;
-  users: Array<User>;
-};
+  __typename?: 'Query'
+  comments: Array<Comment>
+  ok: Scalars['Boolean']
+  posts: Array<Post>
+  projects: Array<Project>
+  users: Array<User>
+}
 
 export type User = {
-  __typename?: 'User';
-  comments: Array<Comment>;
-  createdAt: Scalars['Date'];
-  email: Scalars['String'];
-  id: Scalars['ID'];
-  name?: Maybe<Scalars['String']>;
-  posts: Array<Post>;
-  updatedAt: Scalars['Date'];
-};
+  __typename?: 'User'
+  comments: Array<Comment>
+  createdAt: Scalars['Date']
+  email: Scalars['String']
+  id: Scalars['ID']
+  name?: Maybe<Scalars['String']>
+  posts: Array<Post>
+  updatedAt: Scalars['Date']
+}
 
 export type CreatePostMutationVariables = Exact<{
-  title: Scalars['String'];
-  content: Scalars['String'];
-  slug: Scalars['String'];
-  authorId: Scalars['ID'];
-  description: Scalars['String'];
-}>;
+  title: Scalars['String']
+  content: Scalars['String']
+  slug: Scalars['String']
+  authorId: Scalars['ID']
+  description: Scalars['String']
+}>
 
+export type CreatePostMutation = {
+  __typename?: 'Mutation'
+  createPost: {
+    __typename?: 'Post'
+    title: string
+    content: string
+    slug: string
+    description: string
+    author: { __typename?: 'User'; id: string }
+  }
+}
 
-export type CreatePostMutation = { __typename?: 'Mutation', createPost: { __typename?: 'Post', title: string, content: string, slug: string, description: string, author: { __typename?: 'User', id: string } } };
+export type PostQueryVariables = Exact<{ [key: string]: never }>
 
-export type PostQueryVariables = Exact<{ [key: string]: never; }>;
+export type PostQuery = {
+  __typename?: 'Query'
+  posts: Array<{
+    __typename?: 'Post'
+    id: string
+    slug: string
+    createdAt: any
+    updatedAt: any
+    title: string
+    content: string
+    description: string
+    author: { __typename?: 'User'; name?: string | null }
+    comments: Array<{ __typename?: 'Comment'; id: string }>
+  }>
+}
 
+export type ProjectQueryVariables = Exact<{ [key: string]: never }>
 
-export type PostQuery = { __typename?: 'Query', posts: Array<{ __typename?: 'Post', id: string, slug: string, createdAt: any, updatedAt: any, title: string, content: string, description: string, author: { __typename?: 'User', name?: string | null }, comments: Array<{ __typename?: 'Comment', id: string }> }> };
+export type ProjectQuery = {
+  __typename?: 'Query'
+  projects: Array<{
+    __typename?: 'Project'
+    id: string
+    link: string
+    description: string
+    name: string
+    createdAt: any
+    updatedAt: any
+    cover: string
+  }>
+}
 
-export type ProjectQueryVariables = Exact<{ [key: string]: never; }>;
-
-
-export type ProjectQuery = { __typename?: 'Query', projects: Array<{ __typename?: 'Project', id: string, link: string, description: string, name: string, createdAt: any, updatedAt: any, cover: string }> };
-
-
-export const CreatePostDocument = {"kind":"Document","definitions":[{"kind":"OperationDefinition","operation":"mutation","name":{"kind":"Name","value":"CreatePost"},"variableDefinitions":[{"kind":"VariableDefinition","variable":{"kind":"Variable","name":{"kind":"Name","value":"title"}},"type":{"kind":"NonNullType","type":{"kind":"NamedType","name":{"kind":"Name","value":"String"}}}},{"kind":"VariableDefinition","variable":{"kind":"Variable","name":{"kind":"Name","value":"content"}},"type":{"kind":"NonNullType","type":{"kind":"NamedType","name":{"kind":"Name","value":"String"}}}},{"kind":"VariableDefinition","variable":{"kind":"Variable","name":{"kind":"Name","value":"slug"}},"type":{"kind":"NonNullType","type":{"kind":"NamedType","name":{"kind":"Name","value":"String"}}}},{"kind":"VariableDefinition","variable":{"kind":"Variable","name":{"kind":"Name","value":"authorId"}},"type":{"kind":"NonNullType","type":{"kind":"NamedType","name":{"kind":"Name","value":"ID"}}}},{"kind":"VariableDefinition","variable":{"kind":"Variable","name":{"kind":"Name","value":"description"}},"type":{"kind":"NonNullType","type":{"kind":"NamedType","name":{"kind":"Name","value":"String"}}}}],"selectionSet":{"kind":"SelectionSet","selections":[{"kind":"Field","name":{"kind":"Name","value":"createPost"},"arguments":[{"kind":"Argument","name":{"kind":"Name","value":"title"},"value":{"kind":"Variable","name":{"kind":"Name","value":"title"}}},{"kind":"Argument","name":{"kind":"Name","value":"content"},"value":{"kind":"Variable","name":{"kind":"Name","value":"content"}}},{"kind":"Argument","name":{"kind":"Name","value":"slug"},"value":{"kind":"Variable","name":{"kind":"Name","value":"slug"}}},{"kind":"Argument","name":{"kind":"Name","value":"authorId"},"value":{"kind":"Variable","name":{"kind":"Name","value":"authorId"}}},{"kind":"Argument","name":{"kind":"Name","value":"description"},"value":{"kind":"Variable","name":{"kind":"Name","value":"description"}}}],"selectionSet":{"kind":"SelectionSet","selections":[{"kind":"Field","name":{"kind":"Name","value":"title"}},{"kind":"Field","name":{"kind":"Name","value":"content"}},{"kind":"Field","name":{"kind":"Name","value":"slug"}},{"kind":"Field","name":{"kind":"Name","value":"description"}},{"kind":"Field","name":{"kind":"Name","value":"author"},"selectionSet":{"kind":"SelectionSet","selections":[{"kind":"Field","name":{"kind":"Name","value":"id"}}]}}]}}]}}]} as unknown as DocumentNode<CreatePostMutation, CreatePostMutationVariables>;
-export const PostDocument = {"kind":"Document","definitions":[{"kind":"OperationDefinition","operation":"query","name":{"kind":"Name","value":"Post"},"selectionSet":{"kind":"SelectionSet","selections":[{"kind":"Field","name":{"kind":"Name","value":"posts"},"selectionSet":{"kind":"SelectionSet","selections":[{"kind":"Field","name":{"kind":"Name","value":"id"}},{"kind":"Field","name":{"kind":"Name","value":"slug"}},{"kind":"Field","name":{"kind":"Name","value":"createdAt"}},{"kind":"Field","name":{"kind":"Name","value":"updatedAt"}},{"kind":"Field","name":{"kind":"Name","value":"title"}},{"kind":"Field","name":{"kind":"Name","value":"content"}},{"kind":"Field","name":{"kind":"Name","value":"description"}},{"kind":"Field","name":{"kind":"Name","value":"author"},"selectionSet":{"kind":"SelectionSet","selections":[{"kind":"Field","name":{"kind":"Name","value":"name"}}]}},{"kind":"Field","name":{"kind":"Name","value":"comments"},"selectionSet":{"kind":"SelectionSet","selections":[{"kind":"Field","name":{"kind":"Name","value":"id"}}]}}]}}]}}]} as unknown as DocumentNode<PostQuery, PostQueryVariables>;
-export const ProjectDocument = {"kind":"Document","definitions":[{"kind":"OperationDefinition","operation":"query","name":{"kind":"Name","value":"Project"},"selectionSet":{"kind":"SelectionSet","selections":[{"kind":"Field","name":{"kind":"Name","value":"projects"},"selectionSet":{"kind":"SelectionSet","selections":[{"kind":"Field","name":{"kind":"Name","value":"id"}},{"kind":"Field","name":{"kind":"Name","value":"link"}},{"kind":"Field","name":{"kind":"Name","value":"description"}},{"kind":"Field","name":{"kind":"Name","value":"name"}},{"kind":"Field","name":{"kind":"Name","value":"createdAt"}},{"kind":"Field","name":{"kind":"Name","value":"updatedAt"}},{"kind":"Field","name":{"kind":"Name","value":"cover"}}]}}]}}]} as unknown as DocumentNode<ProjectQuery, ProjectQueryVariables>;
+export const CreatePostDocument = {
+  kind: 'Document',
+  definitions: [
+    {
+      kind: 'OperationDefinition',
+      operation: 'mutation',
+      name: { kind: 'Name', value: 'CreatePost' },
+      variableDefinitions: [
+        {
+          kind: 'VariableDefinition',
+          variable: {
+            kind: 'Variable',
+            name: { kind: 'Name', value: 'title' }
+          },
+          type: {
+            kind: 'NonNullType',
+            type: { kind: 'NamedType', name: { kind: 'Name', value: 'String' } }
+          }
+        },
+        {
+          kind: 'VariableDefinition',
+          variable: {
+            kind: 'Variable',
+            name: { kind: 'Name', value: 'content' }
+          },
+          type: {
+            kind: 'NonNullType',
+            type: { kind: 'NamedType', name: { kind: 'Name', value: 'String' } }
+          }
+        },
+        {
+          kind: 'VariableDefinition',
+          variable: { kind: 'Variable', name: { kind: 'Name', value: 'slug' } },
+          type: {
+            kind: 'NonNullType',
+            type: { kind: 'NamedType', name: { kind: 'Name', value: 'String' } }
+          }
+        },
+        {
+          kind: 'VariableDefinition',
+          variable: {
+            kind: 'Variable',
+            name: { kind: 'Name', value: 'authorId' }
+          },
+          type: {
+            kind: 'NonNullType',
+            type: { kind: 'NamedType', name: { kind: 'Name', value: 'ID' } }
+          }
+        },
+        {
+          kind: 'VariableDefinition',
+          variable: {
+            kind: 'Variable',
+            name: { kind: 'Name', value: 'description' }
+          },
+          type: {
+            kind: 'NonNullType',
+            type: { kind: 'NamedType', name: { kind: 'Name', value: 'String' } }
+          }
+        }
+      ],
+      selectionSet: {
+        kind: 'SelectionSet',
+        selections: [
+          {
+            kind: 'Field',
+            name: { kind: 'Name', value: 'createPost' },
+            arguments: [
+              {
+                kind: 'Argument',
+                name: { kind: 'Name', value: 'title' },
+                value: {
+                  kind: 'Variable',
+                  name: { kind: 'Name', value: 'title' }
+                }
+              },
+              {
+                kind: 'Argument',
+                name: { kind: 'Name', value: 'content' },
+                value: {
+                  kind: 'Variable',
+                  name: { kind: 'Name', value: 'content' }
+                }
+              },
+              {
+                kind: 'Argument',
+                name: { kind: 'Name', value: 'slug' },
+                value: {
+                  kind: 'Variable',
+                  name: { kind: 'Name', value: 'slug' }
+                }
+              },
+              {
+                kind: 'Argument',
+                name: { kind: 'Name', value: 'authorId' },
+                value: {
+                  kind: 'Variable',
+                  name: { kind: 'Name', value: 'authorId' }
+                }
+              },
+              {
+                kind: 'Argument',
+                name: { kind: 'Name', value: 'description' },
+                value: {
+                  kind: 'Variable',
+                  name: { kind: 'Name', value: 'description' }
+                }
+              }
+            ],
+            selectionSet: {
+              kind: 'SelectionSet',
+              selections: [
+                { kind: 'Field', name: { kind: 'Name', value: 'title' } },
+                { kind: 'Field', name: { kind: 'Name', value: 'content' } },
+                { kind: 'Field', name: { kind: 'Name', value: 'slug' } },
+                { kind: 'Field', name: { kind: 'Name', value: 'description' } },
+                {
+                  kind: 'Field',
+                  name: { kind: 'Name', value: 'author' },
+                  selectionSet: {
+                    kind: 'SelectionSet',
+                    selections: [
+                      { kind: 'Field', name: { kind: 'Name', value: 'id' } }
+                    ]
+                  }
+                }
+              ]
+            }
+          }
+        ]
+      }
+    }
+  ]
+} as unknown as DocumentNode<CreatePostMutation, CreatePostMutationVariables>
+export const PostDocument = {
+  kind: 'Document',
+  definitions: [
+    {
+      kind: 'OperationDefinition',
+      operation: 'query',
+      name: { kind: 'Name', value: 'Post' },
+      selectionSet: {
+        kind: 'SelectionSet',
+        selections: [
+          {
+            kind: 'Field',
+            name: { kind: 'Name', value: 'posts' },
+            selectionSet: {
+              kind: 'SelectionSet',
+              selections: [
+                { kind: 'Field', name: { kind: 'Name', value: 'id' } },
+                { kind: 'Field', name: { kind: 'Name', value: 'slug' } },
+                { kind: 'Field', name: { kind: 'Name', value: 'createdAt' } },
+                { kind: 'Field', name: { kind: 'Name', value: 'updatedAt' } },
+                { kind: 'Field', name: { kind: 'Name', value: 'title' } },
+                { kind: 'Field', name: { kind: 'Name', value: 'content' } },
+                { kind: 'Field', name: { kind: 'Name', value: 'description' } },
+                {
+                  kind: 'Field',
+                  name: { kind: 'Name', value: 'author' },
+                  selectionSet: {
+                    kind: 'SelectionSet',
+                    selections: [
+                      { kind: 'Field', name: { kind: 'Name', value: 'name' } }
+                    ]
+                  }
+                },
+                {
+                  kind: 'Field',
+                  name: { kind: 'Name', value: 'comments' },
+                  selectionSet: {
+                    kind: 'SelectionSet',
+                    selections: [
+                      { kind: 'Field', name: { kind: 'Name', value: 'id' } }
+                    ]
+                  }
+                }
+              ]
+            }
+          }
+        ]
+      }
+    }
+  ]
+} as unknown as DocumentNode<PostQuery, PostQueryVariables>
+export const ProjectDocument = {
+  kind: 'Document',
+  definitions: [
+    {
+      kind: 'OperationDefinition',
+      operation: 'query',
+      name: { kind: 'Name', value: 'Project' },
+      selectionSet: {
+        kind: 'SelectionSet',
+        selections: [
+          {
+            kind: 'Field',
+            name: { kind: 'Name', value: 'projects' },
+            selectionSet: {
+              kind: 'SelectionSet',
+              selections: [
+                { kind: 'Field', name: { kind: 'Name', value: 'id' } },
+                { kind: 'Field', name: { kind: 'Name', value: 'link' } },
+                { kind: 'Field', name: { kind: 'Name', value: 'description' } },
+                { kind: 'Field', name: { kind: 'Name', value: 'name' } },
+                { kind: 'Field', name: { kind: 'Name', value: 'createdAt' } },
+                { kind: 'Field', name: { kind: 'Name', value: 'updatedAt' } },
+                { kind: 'Field', name: { kind: 'Name', value: 'cover' } }
+              ]
+            }
+          }
+        ]
+      }
+    }
+  ]
+} as unknown as DocumentNode<ProjectQuery, ProjectQueryVariables>
